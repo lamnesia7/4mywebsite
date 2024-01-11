4af6becbae666a01f6ff58fbfff9579bf5d2492a
lamnesia7 <145881131+lamnesia7@users.noreply.github.com>
2024-01-10 19:31:31 -0500

    Add files via upload

---
+++ motion of accordion collapsible lis.js
@@ -0,0 +1,25 @@
+//motion of accordion collapsible lists
+$('.title').on('click', function() {
+	$('.box').slideUp(500);
+    
+	var findElm = $(this).next(".box");
+    
+	if($(this).hasClass('close')){
+		$(this).removeClass('close');    
+	}else{//それ以外は
+		$('.close').removeClass('close'); 
+		$(this).addClass('close');
+		$(findElm).slideDown(500);
+	}
+});
+
+
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); 
+	$(".open").each(function(index, element){	
+		var Title =$(element).children('.title');	
+		$(Title).addClass('close');				
+		var Box =$(element).children('.box');	
+		$(Box).slideDown(500);					
+	});
+});
